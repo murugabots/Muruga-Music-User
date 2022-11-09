f2e28df5b43f149457c57246479a7feaf46db780
murugabots <115962233+murugabots@users.noreply.github.com>
2022-11-09 07:13:06 -0800

    Muruga

---
+++ ABG/helper.py
@@ -0,0 +1,6 @@
+HELP_IMG = ["https://telegra.ph/file/83d3d122a961203de05ab.jpg"]
+
+START_IMG = ["https://telegra.ph/file/83d3d122a961203de05ab.jpg"]
+
+
+PHOTO = ["https://telegra.ph/file/83d3d122a961203de05ab.jpg"]
